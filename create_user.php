59639673970b2945d8e0c4da45e5d18ef8aaf41d
ulisses.gomes <ulisses@voxtecnologia.com.br>
2024-08-28 21:14:23 -0300

    ajuste create_user.php pdo

--- create_user.php
+++ create_user.php
@@ -6,11 +6,9 @@ $nome=$_POST['nome'];
 $email=$_POST['email'];
 $senha=$_POST['senha'];
 try{
-
-	$myPDO =new PDO("pgsql:host=172.17.0.3;dbname=postgres","postgres","teupaiandadeuno");
 	
 	$sql_query1 ="INSERT INTO usuario(nome,email,senha,data_criacao)values('$nome','$email',$senha,CURRENT_TIMESTAMP)";
-	$myPDO->query($sql_query1);
+	$pdo->query($sql_query1);
 
 	echo "Usuário cadastrado com sucesso.";
 	
